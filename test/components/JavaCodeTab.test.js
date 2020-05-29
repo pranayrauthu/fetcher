a32d4fa51ad9bb839786a3edfe2386eac33a952b
pranayrauthu <pranay.rauthu@gmail.com>
2020-05-29 20:50:18 +0530

    fix java e2e tests

--- test/components/JavaCodeTab.test.js
+++ test/components/JavaCodeTab.test.js
@@ -20,7 +20,32 @@ beforeEach(async () => {
 test('JavaCodeTab should render on load', async () => {
 
     const codeSnippet = await commonInputs.getCodeSnippet(page, 'java');
-    expect(codeSnippet).toBe(`import java.io.DataOutputStream;import java.io.BufferedReader;import java.io.InputStreamReader;import java.net.URL;import java.net.HttpURLConnection;​public class Main {  public static void main(String[] args) {    try {      URL url = new URL("");      HttpURLConnection connection = (HttpURLConnection) url.openConnection();      connection.setRequestMethod("GET");      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));      String inputLine;      StringBuffer response = new StringBuffer();      while ((inputLine = in.readLine()) != null) {        response.append(inputLine);      }      in.close();      System.out.println(response.toString());    } catch (Exception e) {      // TODO: handle exception    }  }}`);
+    const expectedCodeSnippet = `import java.io.DataOutputStream;` +
+                                `import java.io.BufferedReader;` +
+                                `import java.io.InputStreamReader;` +
+                                `import java.net.URL;` +
+                                `import java.net.HttpURLConnection;​` +
+                                `public class App {` +
+                                `  public static void main(String[] args) {` +
+                                `    try {` +
+                                `      URL url = new URL("");` +
+                                `      HttpURLConnection connection = (HttpURLConnection) url.openConnection();` +
+                                `      connection.setRequestMethod("GET");` +
+                                `      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));` +
+                                `      String inputLine;` +
+                                `      StringBuffer response = new StringBuffer();` +
+                                `      while ((inputLine = in.readLine()) != null) {` +
+                                `        response.append(inputLine);` +
+                                `      }` +
+                                `      in.close();` +
+                                `      System.out.println(response.toString());` +
+                                `    }` +
+                                ` catch (Exception e) {` +
+                                `      // TODO: handle exception` +
+                                `    }` +
+                                `  }` +
+                                `}​`;
+    expect(codeSnippet).toEqual(expectedCodeSnippet);
     
 }, 30000);
 
@@ -28,7 +53,33 @@ test('JavaCodeTab should render GET request code', async () => {
 
     await commonInputs.enterSampleGETRequestInput(page);
     let codeSnippet = await commonInputs.getCodeSnippet(page, 'java');
-    expect(codeSnippet).toBe(`import java.io.DataOutputStream;import java.io.BufferedReader;import java.io.InputStreamReader;import java.net.URL;import java.net.HttpURLConnection;​public class Main {  public static void main(String[] args) {    try {      URL url = new URL("https://jsonplaceholder.typicode.com/posts/1");      HttpURLConnection connection = (HttpURLConnection) url.openConnection();      connection.setRequestMethod("GET");      connection.setRequestProperty("Accept", "application/json");      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));      String inputLine;      StringBuffer response = new StringBuffer();      while ((inputLine = in.readLine()) != null) {        response.append(inputLine);      }      in.close();      System.out.println(response.toString());    } catch (Exception e) {      // TODO: handle exception    }  }}`);
+    const expectedCodeSnippet = `import java.io.DataOutputStream;` +
+                                `import java.io.BufferedReader;` +
+                                `import java.io.InputStreamReader;` +
+                                `import java.net.URL;` +
+                                `import java.net.HttpURLConnection;` +
+                                `​public class App {` +
+                                `  public static void main(String[] args) {` +
+                                `    try {` +
+                                `      URL url = new URL(\"https://jsonplaceholder.typicode.com/posts/1\");` +
+                                `      HttpURLConnection connection = (HttpURLConnection) url.openConnection();` +
+                                `      connection.setRequestMethod(\"GET\");` +
+                                `      connection.setRequestProperty(\"Accept\", \"application/json\");` +
+                                `      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));` +
+                                `      String inputLine;` +
+                                `      StringBuffer response = new StringBuffer();` +
+                                `      while ((inputLine = in.readLine()) != null) {` +
+                                `        response.append(inputLine);` +
+                                `      }` +
+                                `      in.close();` +
+                                `      System.out.println(response.toString());` +
+                                `    }` +
+                                ` catch (Exception e) {` +
+                                `      // TODO: handle exception    }` +
+                                `  }` +
+                                `}` +
+                                `​`;
+    expect(codeSnippet).toEqual(expectedCodeSnippet);
 
 }, 30000);
 
@@ -36,9 +87,40 @@ test('JavaCodeTab should render POST request code', async () => {
 
     await commonInputs.enterSamplePOSTRequestInput(page);
     let codeSnippet = await commonInputs.getCodeSnippet(page, 'java');
-    expect(codeSnippet).toBe(`import java.io.DataOutputStream;import java.io.BufferedReader;import java.io.InputStreamReader;import java.net.URL;import java.net.HttpURLConnection;​public class Main {  public static void main(String[] args) {    try {      URL url = new URL("https://jsonplaceholder.typicode.com/posts");      HttpURLConnection connection = (HttpURLConnection) url.openConnection();      connection.setRequestMethod("POST");      connection.setRequestProperty("Accept", "application/json");      String requestBody = "{\\\"title\\\":\\\"foo\\\",\\\"body\\\":\\\"bar\\\",\\\"userId\\\":1}";      connection.setDoOutput(true);      DataOutputStream wr = new DataOutputStream(connection.getOutputStream());      wr.writeBytes(requestBody);      wr.flush();      wr.close();      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));      String inputLine;      StringBuffer response = new StringBuffer();      while ((inputLine = in.readLine()) != null) {        response.append(inputLine);      }      in.close();      System.out.println(response.toString());    } catch (Exception e) {      // TODO: handle exception`);
+    const expectedCodeSnippet = `import java.io.DataOutputStream;` +
+                                `import java.io.BufferedReader;` +
+                                `import java.io.InputStreamReader;` +
+                                `import java.net.URL;` +
+                                `import java.net.HttpURLConnection;` +
+                                `​public class App {` +
+                                `  public static void main(String[] args) {` +
+                                `    try {` +
+                                `      URL url = new URL("https://jsonplaceholder.typicode.com/posts");` +
+                                `      HttpURLConnection connection = (HttpURLConnection) url.openConnection();` +
+                                `      connection.setRequestMethod("POST");` +
+                                `      connection.setRequestProperty("Accept", "application/json");` +
+                                `      String requestBody = "{\\\"title\\\":\\\"foo\\\",\\\"body\\\":\\\"bar\\\",\\\"userId\\\":1}";` +
+                                `      connection.setDoOutput(true);` +
+                                `      DataOutputStream wr = new DataOutputStream(connection.getOutputStream());` +
+                                `      wr.writeBytes(requestBody);` +
+                                `      wr.flush();` +
+                                `      wr.close();` +
+                                `      BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));` +
+                                `      String inputLine;` +
+                                `      StringBuffer response = new StringBuffer();` +
+                                `      while ((inputLine = in.readLine()) != null) {` +
+                                `        response.append(inputLine);` +
+                                `      }` +
+                                `      in.close();` +
+                                `      System.out.println(response.toString());` +
+                                `    }` +
+                                ` catch (Exception e) {` +
+                                `      // TODO: handle exception`;
+    expect(codeSnippet).toBe(expectedCodeSnippet);
 
 }, 30000);
+
+// TODO: Need to add e2e tests for javacode tab
 
 afterAll(() => {
     browser.close();
